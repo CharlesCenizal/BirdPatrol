0730ee67b0fd7abb66aefbffc071cde56378eee4
charlescenizal <charles442009@gmail.com>
2021-04-21 02:25:05 -0700

    added pew sound

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,7 +5,7 @@ class Menu extends Phaser.Scene {
     preload() {
         this.load.audio('sfx_select', './assets/assets_blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/assets_explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/assets_rocket_shot.wav');
+        this.load.audio('sfx_rocket', './assets/pew.mp3');
     }
     // adding the menu
     create() {
